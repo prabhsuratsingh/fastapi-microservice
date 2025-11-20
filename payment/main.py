e306885aa61d82f1126960b78bbadc27d8e898c9
prabhsuratsingh <prabhsurat17@gmail.com>
2025-11-21 01:06:25 +0530

    init payment microservice

---
+++ payment/main.py
@@ -0,0 +1,52 @@
+import re
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from redis_om import get_redis_connection, HashModel
+from starlette.requests import Request
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+REDIS_HOST = os.getenv("REDIS_HOST")
+REDIS_PORT = os.getenv("REDIS_PORT")
+REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+redis = get_redis_connection(
+    host=REDIS_HOST,
+    port=REDIS_PORT,
+    password=REDIS_PASSWORD,
+    decode_responses=True,
+)
+
+class Order(HashModel):
+    product_id: str
+    price: float
+    fee: float
+    total: float
+    quantity: int
+    status: str 
+
+    class Meta:
+        database = redis
+
+@app.post("/orders")
+async def create(request: Request):
+    body = await request.json()
+
+    req = requests.get("http://localhost:8000/products/%s" % body["product_id"])
+
+    return req.json()
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="localhost", port=8001)
