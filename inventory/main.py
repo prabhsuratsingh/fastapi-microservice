637047c9d3437241a8d72fa76744de688c608041
prabhsuratsingh <prabhsurat17@gmail.com>
2025-11-20 23:55:06 +0530

    create inventory microservice

--- inventory/main.py
+++ inventory/main.py
@@ -1,6 +1,65 @@
-def main():
-    print("Hello from inventory!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from redis_om import get_redis_connection, HashModel
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+REDIS_HOST = os.getenv("REDIS_HOST")
+REDIS_PORT = os.getenv("REDIS_PORT")
+REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+redis = get_redis_connection(
+    host=REDIS_HOST,
+    port=REDIS_PORT,
+    password=REDIS_PASSWORD,
+    decode_responses=True,
+) 
+
+class Product(HashModel):
+    name: str
+    price: float
+    quantity: int
+    
+    class Meta:
+        database=redis
+
+@app.get("/products")
+def all():
+    return [fromat(pk) for pk in Product.all_pks()]
+
+def fromat(pk: str):
+    product = Product.get(pk)
+
+    return {
+        'id': product.pk,
+        'name': product.name,
+        'price': product.price,
+        'quantity': product.quantity
+    }
+
+@app.post("/products")
+def create(product: Product):
+    return product.save()
+
+@app.get("/products/{pk}")
+def get(pk: str):
+    return Product.get(pk)
+
+@app.delete("/products/{pk}")
+def delete(pk: str):
+    return Product.delete(pk)
 
 
 if __name__ == "__main__":
-    main()
+    import uvicorn
+    uvicorn.run(app, host="localhost", port=8000)
