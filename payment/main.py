a89fb41861f455c5dc15ed2bd6d666875f7c0a13
prabhsuratsingh <prabhsurat17@gmail.com>
2025-11-21 18:40:53 +0530

    create background task

--- payment/main.py
+++ payment/main.py
@@ -1,6 +1,8 @@
 import re
+import time
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.background import BackgroundTasks
 from redis_om import get_redis_connection, HashModel
 from starlette.requests import Request
 import requests
@@ -39,13 +41,34 @@ class Order(HashModel):
     class Meta:
         database = redis
 
-@app.post("/orders")
-async def create(request: Request):
+@app.get("/orders/{pk}")
+def get(pk: str):
+    return Order.get(pk)
+
+@app.post("/orders")  
+async def create(request: Request, background_tasks: BackgroundTasks):
     body = await request.json()
 
-    req = requests.get("http://localhost:8000/products/%s" % body["product_id"])
+    req = requests.get("http://localhost:8000/products/%s" % body["id"])
+    product = req.json()
 
-    return req.json()
+    order = Order(
+        product_id=body["id"],
+        price=product["price"],
+        fee=0.2 * product['price'],
+        total=1.2 * product['price'],
+        quantity=body["quantity"],
+        status='pending'
+    )
+    order.save()
+    background_tasks.add_task(order_completed, order)
+
+    return order
+
+def order_completed(order: Order):
+    time.sleep(5)
+    order.status = 'completed'
+    order.save()
 
 if __name__ == "__main__":
     import uvicorn
